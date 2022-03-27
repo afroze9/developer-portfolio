e56904fc2a8a323520de4112c6ab307d911828ea
afroze9 <afrozeamjad@gmail.com>
2022-03-27 19:33:31 +0500

    Initial setup complete

---
+++ src/components/Attributions.tsx
@@ -0,0 +1,9 @@
+const Attributions = () => {
+  return (
+    <div>
+      <a href="https://www.flaticon.com/free-icons/portfolio" title="portfolio icons">Portfolio icons created by Smashicons - Flaticon</a>
+    </div>
+  )
+}
+
+export default Attributions;
